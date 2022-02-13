08e587a9ad8f79ab2dd5eccfb14ff086a33b6eb2
Marcin Janiczek <inspiremarcin@gmail.com>
2022-02-13 03:41:56 +0100

    Initial Commit

---
+++ Planets_3D/Planets_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planets_3DApp.swift
+//  Planets_3D
+//
+//  Created by Marcin on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Planets_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
